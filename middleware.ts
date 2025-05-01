cc7008b8aa73b0f4321a15cddecc8395c35ab05d
Meet Zinzuvadiya <zinzuvadiyameet98@email.com>
2025-05-01 11:22:19 +0530

    update: middleware for bypass auth

--- middleware.ts
+++ middleware.ts
@@ -3,17 +3,17 @@ import { NextResponse } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const isAuthenticated = request.cookies.has('auth-token') // You can change this based on your auth implementation
+  // const isAuthenticated = request.cookies.has('auth-token') // You can change this based on your auth implementation
 
-  // If the user is not authenticated and trying to access protected routes
-  if (!isAuthenticated && !request.nextUrl.pathname.startsWith('/auth')) {
-    return NextResponse.redirect(new URL('/auth', request.url))
-  }
+  // // If the user is not authenticated and trying to access protected routes
+  // if (!isAuthenticated && !request.nextUrl.pathname.startsWith('/auth')) {
+  //   return NextResponse.redirect(new URL('/auth', request.url))
+  // }
 
-  // If the user is authenticated and trying to access login page
-  if (isAuthenticated && request.nextUrl.pathname.startsWith('/auth')) {
-    return NextResponse.redirect(new URL('/', request.url))
-  }
+  // // If the user is authenticated and trying to access login page
+  // if (isAuthenticated && request.nextUrl.pathname.startsWith('/auth')) {
+  //   return NextResponse.redirect(new URL('/', request.url))
+  // }
 
   return NextResponse.next()
 }
